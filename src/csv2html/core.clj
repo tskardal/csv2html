1b30479cd78edfcd6eafa9e64942ccf7ff63b135
Thomas Skardal <thomas.skardal@gmail.com>
2015-02-01 22:38:39 +0100

    Parsing headers and values

--- src/csv2html/core.clj
+++ src/csv2html/core.clj
@@ -1,6 +1,15 @@
-(ns csv2html.core)
+(ns csv2html.core
+  (:require [clojure.string :as str]))
 
-(defn parse-csv 
-  "doc-string"
-  [csv-str]
-  )
+(defn parse-csv [csv-str]
+  (let [lines (str/split-lines csv-str)
+        headers (str/split (first lines) #",")
+        values (rest lines)]    
+    {:headers  headers :values (vec (map #(str/split % #",") values))}))
+
+(defn to-html [headers values]
+  (let [pairs (map vector headers values)]
+    (for [[h p] pairs]
+      (str "<h3>" h "</h3><p>" p "</p>"))))
+
+
--- test/csv2html/core_test.clj
+++ test/csv2html/core_test.clj
@@ -2,6 +2,12 @@
   (:use midje.sweet)
   (:use [csv2html.core]))
 
-(facts "about `parse-csv`"
-  (fact "it sucks"
-    (parse-csv "") => nil))
+(facts "about `parse-csv`"  
+  (fact "it expects the first row to be column headers"
+        (:headers (parse-csv "a,b")) => ["a" "b"])
+  (fact "all but the first row are value rows"
+        (:values (parse-csv "a,b\n0,1\n2,3")) => [["0" "1"] ["2" "3"]]))
+
+(comment (facts "about `to-pages`"
+                 (fact "it should create one page for each value row"
+                       (to-pages (parse-csv "a,b\n0,1\n2,3")) => [])))
