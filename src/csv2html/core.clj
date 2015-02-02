2acf50675d588f6c24d30f95ed9d03b40cb51a66
Thomas Skardal <thomas.skardal@gmail.com>
2015-02-02 20:51:59 +0100

    Kind of working buggy first version

--- src/csv2html/core.clj
+++ src/csv2html/core.clj
@@ -1,5 +1,6 @@
 (ns csv2html.core
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str])
+  (:gen-class))
 
 (defn parse-csv [csv-str]
   (let [lines (str/split-lines csv-str)
@@ -12,4 +13,15 @@
     (for [[h p] pairs]
       (str "<h3>" h "</h3><p>" p "</p>"))))
 
+(defn csv-to-html [in]
+  (let [s (slurp in)
+        parsed (parse-csv s)
+        headers (:headers parsed)
+        values (:values parsed)]
+    (->> values
+         (map #(to-html headers %))
+         flatten)))
 
+(defn -main [& args]
+  (let [result (flatten (csv-to-html (first args)))]    
+    (spit (second args) (apply str result))))
--- test/csv2html/core_test.clj
+++ test/csv2html/core_test.clj
@@ -7,7 +7,3 @@
         (:headers (parse-csv "a,b")) => ["a" "b"])
   (fact "all but the first row are value rows"
         (:values (parse-csv "a,b\n0,1\n2,3")) => [["0" "1"] ["2" "3"]]))
-
-(comment (facts "about `to-pages`"
-                 (fact "it should create one page for each value row"
-                       (to-pages (parse-csv "a,b\n0,1\n2,3")) => [])))
